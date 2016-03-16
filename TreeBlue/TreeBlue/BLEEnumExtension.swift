48b043bb964bfe8709be27f26e5f536d1419a20b
wwwins <jacky.huang@isobar.com>
2016-03-16 11:42:58 +0800

    Minor updates.

--- TreeBlue/TreeBlue/BLEEnumExtension.swift
+++ TreeBlue/TreeBlue/BLEEnumExtension.swift
@@ -10,53 +10,59 @@ import CoreBluetooth
 
 extension CBCentralManagerState {
   var description:String {
-    switch self {
-    case .PoweredOff:
-      return "CBCentralManagerStatePoweredOff"
-    case .PoweredOn:
-      return "CBCentralManagerStatePoweredOn"
-    case .Resetting:
-      return "CBCentralManagerStateResetting"
-    case .Unauthorized:
-      return "CBCentralManagerStateUnauthorized"
-    case .Unknown:
-      return "CBCentralManagerStateUnknown"
-    case .Unsupported:
-      return "CBCentralManagerStateUnsupported"
+    get {
+      switch self {
+      case .PoweredOff:
+        return "CBCentralManagerStatePoweredOff"
+      case .PoweredOn:
+        return "CBCentralManagerStatePoweredOn"
+      case .Resetting:
+        return "CBCentralManagerStateResetting"
+      case .Unauthorized:
+        return "CBCentralManagerStateUnauthorized"
+      case .Unknown:
+        return "CBCentralManagerStateUnknown"
+      case .Unsupported:
+        return "CBCentralManagerStateUnsupported"
+      }
     }
   }
 }
 
 extension CBPeripheralState {
   var description:String {
-    switch self {
-    case .Connected:
-      return "CBPeripheralStateConnected"
-    case .Connecting:
-      return "CBPeripheralStateConnecting"
-    case .Disconnected:
-      return "CBPeripheralStateDisconnected"
-    case .Disconnecting:
-      return "CBPeripheralStateDisconnecting"
+    get {
+      switch self {
+      case .Connected:
+        return "CBPeripheralStateConnected"
+      case .Connecting:
+        return "CBPeripheralStateConnecting"
+      case .Disconnected:
+        return "CBPeripheralStateDisconnected"
+      case .Disconnecting:
+        return "CBPeripheralStateDisconnecting"
+      }
     }
   }
 }
 
 extension CBPeripheralManagerState {
   var description:String {
-    switch self {
-    case .PoweredOff:
-      return "CBPeripheralManagerStatePoweredOff"
-    case .PoweredOn:
-      return "CBPeripheralManagerStatePoweredOn"
-    case .Resetting:
-      return "CBPeripheralManagerStateResetting"
-    case .Unauthorized:
-      return "CBPeripheralManagerStateUnauthorized"
-    case .Unknown:
-      return "CBPeripheralManagerStateUnknown"
-    case .Unsupported:
-      return "CBPeripheralManagerStateUnsupported"
+    get {
+      switch self {
+      case .PoweredOff:
+        return "CBPeripheralManagerStatePoweredOff"
+      case .PoweredOn:
+        return "CBPeripheralManagerStatePoweredOn"
+      case .Resetting:
+        return "CBPeripheralManagerStateResetting"
+      case .Unauthorized:
+        return "CBPeripheralManagerStateUnauthorized"
+      case .Unknown:
+        return "CBPeripheralManagerStateUnknown"
+      case .Unsupported:
+        return "CBPeripheralManagerStateUnsupported"
+      }
     }
   }
 }
