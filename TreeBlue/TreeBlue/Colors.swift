477b9ad458a229d762ca3115d314590b2f0547ca
wwwins <jacky.huang@isobar.com>
2016-03-22 10:19:38 +0800

    Added colors.

---
+++ TreeBlue/TreeBlue/Colors.swift
@@ -0,0 +1,19 @@
+//
+//  Colors.swift
+//  TreeBlue
+//
+//  Created by wwwins on 2016/3/21.
+//  Copyright © 2016年 wwwins. All rights reserved.
+//
+
+import UIKit
+
+struct Colors {
+  static let blue = UIColor(red: 46.0 / 255.0, green: 117.0 / 255.0, blue: 146.0 / 255.0, alpha: 1.0)
+  static let red = UIColor(red: 209.0 / 255.0, green: 42.0 / 255.0, blue: 24.0 / 255.0, alpha: 1.0)
+  static let green = UIColor(red: 0.0, green: 142.0/255.0, blue: 0, alpha: 1.0)
+  static let lightGreen = UIColor(red: 0.0, green: 196.0/255.0, blue: 0, alpha: 1.0)
+  static let white = UIColor.whiteColor()
+  static let black = UIColor.blackColor()
+  static let clear = UIColor.clearColor()
+}
